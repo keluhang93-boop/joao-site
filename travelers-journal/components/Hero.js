feb2048446eaf01f8905faf05eaacd93dbdef9c3
keluhang93-boop <keluhang93@gmail.com>
2026-01-16 13:34:19 +0100

    Create Hero.js

---
+++ travelers-journal/components/Hero.js
@@ -0,0 +1,9 @@
+export function Hero() {
+    return `
+        <section class="hero">
+            <h1>Adventure Awaits</h1>
+            <p>Explore the world through my eyes.</p>
+            <button class="main-btn">Start Exploring</button>
+        </section>
+    `;
+}
