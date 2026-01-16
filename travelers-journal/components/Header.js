bbadf4bf9168e35b3d95614845366f54c7385855
keluhang93-boop <keluhang93@gmail.com>
2026-01-16 13:34:07 +0100

    Create Header.js

---
+++ travelers-journal/components/Header.js
@@ -0,0 +1,11 @@
+export function Header() {
+    return `
+        <nav class="navbar">
+            <div class="logo">TRAVLR</div>
+            <ul>
+                <li><a href="#">Destinations</a></li>
+                <li><a href="#">Journal</a></li>
+            </ul>
+        </nav>
+    `;
+}
